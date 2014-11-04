37c3c0e1b174e6cb4605d56129c34173002d78af
Elizabeth Collins <escollins27@gmail.com>
2014-11-04 11:07:13 -0500

    Users can create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,33 @@ class PostsController < ApplicationController
   before_action :require_login
 
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = current_user.posts.new(post_params)
+    if post.save
+      redirect_to post_path(post)
+    else
+      render :new
+    end
+  end
+  
+  def show
+    @post = load_post_from_url
+  end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def load_post_from_url
+    Post.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
-  resources :posts, only: [:index]
+  resources :posts, only: [:index, :new, :create, :show]
 
   root "posts#index"
 end
