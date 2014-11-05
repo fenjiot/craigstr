8cc8b015a21135be6915101a409a1fdbef814624
fenjiot <fenjiot@gmail.com>
2014-11-05 14:06:05 -0500

    WIP browse categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,27 +1,39 @@
 class CategoriesController <ApplicationController
+  before_action :require_admin, except: [:index]
+
   def index
-    @new_category = Category.new
-    @categories = Category.all
+    @region = load_region_from_url
+    @new_category = @region.categories.new
+    @categories = @region.categories.all
   end
 
   def create
-    @new_category = Category.new(category_params)
+    @region = load_region_from_url
+    @new_category = @region.categories.new(category_params)
     if @new_category.save
       redirect_to categories_path
     else
-      @categories = Category.all
+      @categories = @region.categories.all
       render :index
     end
   end
   
   def show
-    @category = load_category_from_url
+    @region = load_region_from_url
+    @category = @region.categories.find(params[:id])
   end
 
   private
 
   def category_params
-    params.require(:category).permit(:text)
+    params.
+      require(:category).
+      permit(:text).
+      merge(region_id: params[:region_id])
+  end
+
+  def load_region_from_url
+    Region.find(params[:region_id])
   end
 
   def load_category_from_url
